4b0830f26a0065a795e15ce2f6aff05d26ca8e56
Saikat Mondal <saikatm1997@gmail.com>
2020-12-29 20:32:38 +0530

    fix

--- minimum-number-swaps-required-sort-array.py
+++ minimum-number-swaps-required-sort-array.py
@@ -1,23 +1,20 @@
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]
+def swap(numbers, i, j):
+    numbers[i], numbers[j] = numbers[j], numbers[i]
 
-
-def minimum_swaps(arr):
-    annotated = [*enumerate(arr)]
+def minimum_swaps(numbers):
+    annotated = [*enumerate(numbers)]
     annotated.sort(key = lambda it: it[1])
 
     count = 0
 
     i = 0
-    while i < len(arr):
+    while i < len(numbers):
         if annotated[i][0] == i:
             i += 1
             continue
         swap(annotated, i, annotated[i][0])
         count += 1
-
     return count
  
-# Driver Code     
-arr = [7, 1, 3, 2, 4, 5, 6]
-print(minimum_swaps(arr))
+numbers = [7, 1, 3, 2, 4, 5, 6]
+print(minimum_swaps(numbers))
